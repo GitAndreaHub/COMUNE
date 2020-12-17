6de864845d6ed0c974953cd75dd872b2e06ba833
GitAndreaHub <s1095511@studenti.univpm.it>
2020-12-17 18:28:52 +0100

    push Andrea

--- SpringBootApp/src/main/java/RaccoltaDati/ChiamataAPI_Weekly.java
+++ SpringBootApp/src/main/java/RaccoltaDati/ChiamataAPI_Weekly.java
@@ -1,5 +1,19 @@
 package RaccoltaDati;
 
-public class ChiamataAPI_Weekly {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ChiamataAPI_Weekly {
+	
+	
+	
 }
---
+++ SpringBootApp/src/main/java/RaccoltaDati/Time_Unix.java
@@ -0,0 +1,11 @@
+package RaccoltaDati;
+
+import java.util.Scanner;
+import java.util.*;
+
+import java.util.Date; // Per avere data in UNIX
+
+public class Time_Unix {
+
+	
+}
