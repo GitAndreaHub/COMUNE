56d8344a883d7b5cca752c055a5c3c8912fd2fce
Stefano Martelli <s1093039@studenti.univpm.it>
2020-12-17 16:52:30 +0100

    Modifica Media Stefano 17 12 2020

--- SpringBootApp/src/main/java/GestioneDati/Media.java
+++ SpringBootApp/src/main/java/GestioneDati/Media.java
@@ -1,5 +1,28 @@
 package GestioneDati;
+import java.util.Vector;
 
 public class Media {
+	private double media;
+	
+	public Media() {
+		
+	}
+	
+	//setter
+	public double MediaNumeri(Vector<Integer>v1){
+		int lunghezza= v1.size();
+		int somma=0;
+		for(int i=0;i<lunghezza; i++) {
+			somma+=v1.get(i);
+		}
+		media=somma/lunghezza;
+		return media;
+	}
+	
+
+	//getter
+	public double GetMedia() {
+		return media;
+	}
 
 }
