502f75a04f4b4d5cab434551ffe28c38637501dc
GitAndreaHub <s1095511@studenti.univpm.it>
2020-12-18 17:02:09 +0100

    Push Andrea 18/12/2020

--- SpringBootApp/src/main/java/RaccoltaDati/ChiamataAPI_Weekly.java
+++ SpringBootApp/src/main/java/RaccoltaDati/ChiamataAPI_Weekly.java
@@ -12,6 +12,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.util.Vector;
 
 public class ChiamataAPI_Weekly {
 	
@@ -27,47 +28,62 @@ public class ChiamataAPI_Weekly {
 		
 	}
 	
-	public void Chiamata_Weekly() {
+	public Vector<Double> Chiamata_Weekly() {
 		
 		JSONParser parser = new JSONParser();
 		
-		try {
+		Vector<Double> temp = new Vector<Double>();
+		
+		for(int u=0; u < 5; u++) {
+		
+			try {
 			
-			CurrentTimeUnix = Time.Get_TimeUnix();
-			//id = ricerca.GetId();
-			lat = ricerca.GetLatitudine();
-			lon = ricerca.GetLongitudine();		
+				CurrentTimeUnix = Time.Get_TimeUnix();
+				CurrentTimeUnix -= 86400; //Si parte dal giorno precedente, altrimente la prima iterazione sara' relativa al giorno stesso e non precedente;
+				
+				lat = ricerca.GetLatitudine();
+				lon = ricerca.GetLongitudine();		
 		
-			URL oracle = new URL("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt=" + CurrentTimeUnix + "&appid=225a3d7ea81634ed4bb00b4cb10f4397");
+				URL oracle = new URL("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt=" + CurrentTimeUnix + "&appid=225a3d7ea81634ed4bb00b4cb10f4397");
 		
-			HttpsURLConnection link = (HttpsURLConnection) oracle.openConnection();
-			link.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+				HttpsURLConnection link = (HttpsURLConnection) oracle.openConnection();
+				link.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
 		
-			BufferedReader input = new BufferedReader(new InputStreamReader(link.getInputStream()));
+				BufferedReader input = new BufferedReader(new InputStreamReader(link.getInputStream()));
 		
-			String inputLine;
-			while((inputLine = input.readLine()) != null) {
 			
-				JSONObject TotalObject = (JSONObject) parser.parse(inputLine);
+				String inputLine;
+				while((inputLine = input.readLine()) != null) {
+			
+					JSONObject TotalObject = (JSONObject) parser.parse(inputLine);
 				
 			
-				JSONArray Hourly = (JSONArray) TotalObject.get("hourly");
-			
-				for(Object o : Hourly) {
+					JSONArray Hourly = (JSONArray) TotalObject.get("hourly");
+				
+				
+					int i=0;
+					for(Object o : Hourly) {
 					
-					JSONObject H = (JSONObject) o;
-					Double temp = (Double) H.get("temp");
+						JSONObject H = (JSONObject) o;
+						temp.set(i, (Double) H.get("temp"));
+						i++;
+					}
+				
 				}
 			
+			} catch (FileNotFoundException e) {
+	            	e.printStackTrace();
+			} catch (IOException e) {
+	            	e.printStackTrace();
+			} catch (ParseException e) {
+	            	e.printStackTrace();
 			}
 		
-		 } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	     } catch (IOException e) {
-	            e.printStackTrace();
-	     } catch (ParseException e) {
-	            e.printStackTrace();
-	     }  
+			CurrentTimeUnix -= 86400;
+		
+		}
+		
+		return temp;
 		
 	}
 	
