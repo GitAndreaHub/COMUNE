7571df9b2b03b97b50857e6eee143cb9c1be26a7
Stefano Martelli <s1093039@studenti.univpm.it>
2021-01-02 11:02:43 +0100

    Eliminazione Classe Estrazione

--- SpringBootApp/src/main/java/GestioneDati/Estrazione.java
+++ SpringBootApp/src/main/java/GestioneDati/Estrazione.java
@@ -1,18 +1,5 @@
 package GestioneDati;
-import java.util.Vector;
 
 public class Estrazione {
-	
-	//costruttore
-	public Estrazione() {
-		
-	}
-	
-	//setter
-	public Double EstraiDato(Vector<Double>v1, int i) {
-		double Dato=0;
-		Dato=v1.get(i);
-		return Dato;
-	}
 
 }
