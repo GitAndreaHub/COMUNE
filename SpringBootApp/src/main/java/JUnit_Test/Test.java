2ffbf35a4ebb60d90dacdbc43955368e30920509
GitAndreaHub <s1095511@studenti.univpm.it>
2021-01-04 16:54:09 +0100

    Inizio JUnit Andrea

---
+++ SpringBootApp/src/main/java/JUnit_Test/Test.java
@@ -0,0 +1,22 @@
+package JUnit_Test;
+
+import RaccoltaDati.RicercaID;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class Test {
+
+	@Test
+	public void TestRicercaID() {
+		
+		//Arrange
+		RicercaID ClassRicerca = new RicercaID();
+		
+		//Assert
+		assertEquals(3183089, ClassRicerca.lettura("Ancona"));
+		
+		
+	}
+}
